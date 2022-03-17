0180718fada48e025979425f45b7b7941d7dd658
Mahrachy Mohamed <mahrachymohamed@gmail.com>
2022-03-17 15:58:21 +0100

    17/03

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
-  // $(".nav-pills > li > button").hover(function () {
-  //   $(this).tab("show");
-  // });
+  // scroll to top
+  var scrollTop = $(".scrollTop");
+
+  $(window).scroll(function () {
+    var topPos = $(this).scrollTop();
+    // if user scrolls down - show scroll to top button
+    if (topPos > 100) {
+      $(scrollTop).css("opacity", "1");
+    } else {
+      $(scrollTop).css("opacity", "0");
+    }
+  });
+
+  $(scrollTop).click(function () {
+    $(window).scrollTop(0);
+  });
 
   const swiperReview = new Swiper(".swiper", {
     spaceBetween: 90,
