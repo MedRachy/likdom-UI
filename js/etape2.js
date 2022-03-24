9a153461a084a472fa6e220bbedd3367b92efe46
Mahrachy Mohamed <mahrachymohamed@gmail.com>
2022-03-24 14:29:14 +0100

    parfum

--- js/etape2.js
+++ js/etape2.js
@@ -154,25 +154,28 @@ function updateProgressbar() {
     ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
 
+// input number + & -
 $(document).on("click", ".custom-input-number .cin-increment", function (e) {
   let $input = $(this).siblings(".cin-input"),
     val = parseInt($input.val()),
     max = parseInt($input.attr("max")),
     step = parseInt($input.attr("step"));
-
   let temp = val + step;
   $input.val(temp <= max ? temp : max);
-  // console.log(temp);
 });
 $(document).on("click", ".custom-input-number .cin-decrement", function (e) {
   let $input = $(this).siblings(".cin-input"),
     val = parseInt($input.val()),
     min = parseInt($input.attr("min")),
     step = parseInt($input.attr("step"));
-
   let temp = val - step;
   $input.val(temp >= min ? temp : min);
-  // console.log(temp);
+});
+
+// select taches check
+$(".div-tache").on("click", function () {
+  var checkbox = $(this).find(".form-check-input");
+  checkbox.prop("checked", !checkbox.prop("checked"));
 });
 
 // select product check
