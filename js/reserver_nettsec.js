0b6789cd295390c6a5ecb130a3aba15287137bac
Mahrachy Mohamed <mahrachymohamed@gmail.com>
2022-04-17 17:37:57 +0000

    nettasec

---
+++ js/reserver_nettsec.js
@@ -0,0 +1,17 @@
+// input number + & -
+$(document).on("click", ".custom-input-number .cin-increment", function (e) {
+  let $input = $(this).siblings(".cin-input"),
+    val = parseInt($input.val()),
+    max = parseInt($input.attr("max")),
+    step = parseInt($input.attr("step"));
+  let temp = val + step;
+  $input.val(temp <= max ? temp : max);
+});
+$(document).on("click", ".custom-input-number .cin-decrement", function (e) {
+  let $input = $(this).siblings(".cin-input"),
+    val = parseInt($input.val()),
+    min = parseInt($input.attr("min")),
+    step = parseInt($input.attr("step"));
+  let temp = val - step;
+  $input.val(temp >= min ? temp : min);
+});
